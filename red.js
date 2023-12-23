2c6b0b4d94381c4b8a9f4b251d64ce719664006c
zhiqin__ <72284220+zhiqin14@users.noreply.github.com>
2023-12-23 00:56:57 +0000

    fix

--- red.js
+++ red.js
@@ -3,7 +3,7 @@ var ua = navigator.userAgent;
 if (ua.includes("iPhone") || ua.includes("Android")){
     location.href = "https://ymzx.qq.com/zlkdatasys/mct/d/play.shtml";
 }
-else if (us.includes("Windows")){
+else if (ua.includes("Windows")){
     location.href = "https://down.pc.yyb.qq.com/channel/1913/2200900000/com_tencent_letsgo_installer.exe"
 }
 else{
