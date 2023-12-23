46bc2cf41cfd5c536f8a6a8224cb97633edf7e40
zhiqin__ <72284220+zhiqin14@users.noreply.github.com>
2023-12-23 00:50:31 +0000

    judge ua

---
+++ red.js
@@ -0,0 +1,11 @@
+var ua = navigator.userAgent;
+
+if (ua.includes("iPhone") || ua.includes("Android")){
+    location.href = "https://ymzx.qq.com/zlkdatasys/mct/d/play.shtml";
+}
+else if (us.includes("Windows")){
+    location.href = "https://down.pc.yyb.qq.com/channel/1913/2200900000/com_tencent_letsgo_installer.exe"
+}
+else{
+    location.herf = "https://gamer.qq.com/v2/cloudgame/game/96897?ichannel=pcgames0Fpcgames1"
+}
